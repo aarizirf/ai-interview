287defd28edf0c23c572d6229304cce71896cbd7
aariz <aarizirf@gmail.com>
2024-12-06 12:05:01 -0500

    changes

--- src/utils/instructions/general.ts
+++ src/utils/instructions/general.ts
@@ -6,10 +6,7 @@ import { questions as evInstructions } from './ev';
 import { questions as accountingInstructions } from './accounting';
 
 export const questions = `
-This is a general interview. Use any of the following questions to conduct the interview. Start with 3 behavioral questions, then move on to the other questions.
-Behavioral Question #1: Something like "Tell me a little about yourself".
-Behavioral Question #2: Something like "Why are you interested in Investment Banking?".
-Behavioral Question #3: Generate one that you think is interesting depending on the candidate's background.
+This is a general interview. Use any of the following questions to conduct the interview.
 ${mergerInstructions}
 
 ${lboInstructions}
